bfc86da402bc3060c3c94cca4e43a1109e99e6f8
Student <example@example.com>
2019-08-19 10:32:48 -0700

    first commit for HTTP Statuses.

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,13 @@
+class QuotesController < ApplicationController
+
+  def index
+    @quotes = {"quotation": "The secret of getting ahead is getting started."}
+    json_response(@quotes)
+  end
+
+
+  private
+  def json_response(object)
+    render json: object, status: :ok
+  end
+end
