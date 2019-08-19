fd0ee1fb872439c0ea8c5a0eb04c3338fa113e12
Student <example@example.com>
2019-08-19 11:20:41 -0700

    It is for Rails Concerns and Ruby Modules - Module and encapsulation.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -27,9 +27,9 @@ class QuotesController < ApplicationController
   end
 
   private
-  def json_response(object, status = :ok)
-    render json: object, status: status
-  end
+  # def json_response(object, status = :ok)
+  #   render json: object, status: status
+  # end
 
   def quote_params
     params.permit(:author, :content)
